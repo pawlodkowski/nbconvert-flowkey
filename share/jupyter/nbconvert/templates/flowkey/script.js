a44dd505e53bd7dcc36c6f70bc72459739a6ae7c
Paul Wlodkowski <pawlodkowski@gmail.com>
2022-03-18 13:24:55 +0100

    js code for retroactively moving executive summary at end into placeholder

--- share/jupyter/nbconvert/templates/flowkey/script.js
+++ share/jupyter/nbconvert/templates/flowkey/script.js
@@ -1,3 +1,7 @@
-var timeStamp = document.getElementById('timestamp_container');
+var timestamp = document.getElementById('timestamp_container');
 var header = document.getElementById('header');
-header.parentNode.insertBefore(timeStamp, header.nextSibling);
+header.parentNode.insertBefore(timestamp, header.nextSibling);
+
+var execSummary = document.getElementById('executive-summary-text');
+var placeholder = document.getElementById('executive-summary-placeholder');
+placeholder.appendChild(execSummary);
